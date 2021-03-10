b3a42dc82a49007923c1ade20fff96960bab38bb
sole <subuchappyv2@gmail.com>
2021-03-10 18:36:15 +0900

    feat: CollectionViewController - configureAutoLayout method 구현 (#11)

--- ProjectManager/ProjectManager/ViewController/ProjectManagerCollectionViewController.swift
+++ ProjectManager/ProjectManager/ViewController/ProjectManagerCollectionViewController.swift
@@ -13,11 +13,24 @@ class ProjectManagerCollectionViewController: UIViewController {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.isScrollEnabled = false
+        collectionView.backgroundColor = .lightGray
         return collectionView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        configureAutoLayout()
+    }
+    
+    private func configureAutoLayout() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
 
