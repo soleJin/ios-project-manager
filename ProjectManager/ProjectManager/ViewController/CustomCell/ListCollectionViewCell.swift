2a9bac0faa34ca73d0df4c162f49ecaf91641a68
odong-Tree <ss12357@naver.com>
2021-03-10 20:13:27 +0900

    feat: ListCollectionViewCell 기본 구현 (#12)

--- ProjectManager/ProjectManager/ViewController/CustomCell/ListCollectionViewCell.swift
+++ ProjectManager/ProjectManager/ViewController/CustomCell/ListCollectionViewCell.swift
@@ -6,4 +6,69 @@
 //
 
 import Foundation
+import UIKit
 
+class ListCollectionViewCell: UICollectionViewCell {
+    var itemList = [Todo]()
+    let tableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configureTableView()
+        configureAutoLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(ListItemTableViewCell.self, forCellReuseIdentifier: "ListTableViewCell")
+        contentView.addSubview(tableView)
+    }
+    
+    private func configureAutoLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
+
+extension ListCollectionViewCell: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return itemList.count
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListTableViewCell") as? ListItemTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        // 임시 데이터
+        let todo = Todo(title: "Hello", description: "Odong", deadLine: Date())
+        let todo2 = Todo(title: "Odongodngodngodngodng", description: "NamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamuNamu", deadLine: Date())
+        
+        if indexPath.row % 2 == 0 {
+            cell.fillLabelsText(item: todo)
+        } else {
+            cell.fillLabelsText(item: todo2)
+        }
+//        cell.fillLabelsText(item: todo)
+//        cell.fillLabelsText(item: itemList[indexPath.row])
+        return cell
+    }
+}
+
+extension ListCollectionViewCell: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
