4c1d37203da40e37815f0be5bf839b7572318017
odong-Tree <ss12357@naver.com>
2021-03-10 20:12:07 +0900

    feat: ProjectManagerCollectionViewController 기본 구현 (#11)

--- ProjectManager/ProjectManager/ViewController/ProjectManagerCollectionViewController.swift
+++ ProjectManager/ProjectManager/ViewController/ProjectManagerCollectionViewController.swift
@@ -7,10 +7,68 @@
 import UIKit
 
 class ProjectManagerCollectionViewController: UIViewController {
+    var statusList = ["TODO", "DOING", "DONE"]
+    let collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+
+        flowLayout.scrollDirection = .horizontal
+        collectionView.isScrollEnabled = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemGray6
+        collectionView.register(ListCollectionViewCell.self, forCellWithReuseIdentifier: "ListCollectionViewCell")
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpCollectionView()
+        configureAutoLayout()
+        configureNavigationBar()
+    }
+    
+    private func setUpCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        view.addSubview(collectionView)
+    }
+    
+    private func configureAutoLayout() {
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    func configureNavigationBar() {
 
     }
 }
+
+extension ProjectManagerCollectionViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return statusList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ListCollectionViewCell", for: indexPath) as? ListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+extension ProjectManagerCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfInterval: CGFloat = CGFloat(10 * (statusList.count - 1))
+        let widthExcludingInterval: CGFloat = collectionView.frame.width - numberOfInterval
+        let cellSize: CGSize = CGSize(width: widthExcludingInterval/3, height: collectionView.frame.height)
+        return cellSize
+    }
+}
+
 
