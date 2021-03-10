453b8c41e311fe9ea22cc2a792f9690a4886f664
sole <subuchappyv2@gmail.com>
2021-03-10 18:02:04 +0900

    feat: navigation controller 설정 (#11)

--- ProjectManager/ProjectManager/SupportingFiles/AppDelegate.swift
+++ ProjectManager/ProjectManager/SupportingFiles/AppDelegate.swift
@@ -14,13 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         if #available(iOS 13.0, *) {
-                    // empty
-                } else {
-                    window = UIWindow()
-                    let rootViewController = ProjectManagerCollectionViewController()
-                    window?.rootViewController = rootViewController
-                    window?.makeKeyAndVisible()
-                }
+            // empty
+        } else {
+            window = UIWindow()
+            let rootViewController = ProjectManagerCollectionViewController()
+            let navigationController = UINavigationController(rootViewController: rootViewController)
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
--- ProjectManager/ProjectManager/SupportingFiles/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SupportingFiles/SceneDelegate.swift
@@ -17,10 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-                window = UIWindow(windowScene: windowScene)
-                let rootViewController = ProjectManagerCollectionViewController()
-                window?.rootViewController = rootViewController
-                window?.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+        let rootViewController = ProjectManagerCollectionViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
