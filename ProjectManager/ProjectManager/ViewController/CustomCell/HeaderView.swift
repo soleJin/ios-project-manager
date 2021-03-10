e376b729b27c3eb5be58648157163622fa3bd0e9
sole <subuchappyv2@gmail.com>
2021-03-11 03:02:55 +0900

    feat: HeaderView - view, labels property 생성

---
+++ ProjectManager/ProjectManager/ViewController/CustomCell/HeaderView.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewHeaderCell.swift
+//  ProjectManager
+//
+//  Created by sole on 2021/03/11.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+    
+    private let containerView = UIView()
+
+    private let headerTitleLabel: UILabel = {
+        let headerTitleLabel = UILabel()
+        headerTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerTitleLabel.font = UIFont.boldSystemFont(ofSize: 35)
+        return headerTitleLabel
+    }()
+    
+    private let listCountLabel: UILabel = {
+        let cellCountLabel = UILabel()
+        cellCountLabel.translatesAutoresizingMaskIntoConstraints = false
+        cellCountLabel.backgroundColor = .black
+        cellCountLabel.textColor = .white
+        cellCountLabel.textAlignment = .center
+        cellCountLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        cellCountLabel.layer.masksToBounds = true
+        cellCountLabel.layer.cornerRadius = 15
+        return cellCountLabel
+    }()
+}
