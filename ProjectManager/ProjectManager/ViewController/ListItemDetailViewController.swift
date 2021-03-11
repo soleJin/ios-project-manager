6ef612f8a3abcd793f026333ac1b225c687761b6
sole <subuchappyv2@gmail.com>
2021-03-11 15:15:31 +0900

    feat: ListItemDetailViewController - textField, datePicker, textView property 생성 (#7)

--- ProjectManager/ProjectManager/ViewController/ListItemDetailViewController.swift
+++ ProjectManager/ProjectManager/ViewController/ListItemDetailViewController.swift
@@ -8,7 +8,42 @@
 import UIKit
 
 class ListItemDetailViewController: UIViewController {
-
+    
+    private let titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont.boldSystemFont(ofSize: 19)
+        textField.placeholder = "Title"
+        textField.borderStyle = .roundedRect
+        //To apply Shadow
+        textField.layer.shadowOpacity = 1
+        textField.layer.shadowColor = UIColor.systemGray5.cgColor
+        textField.layer.shadowOffset = .zero
+        textField.layer.shadowRadius = 3.0
+        //To apply padding
+        let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: textField.frame.height))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+        return textField
+    }()
+    
+    private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        datePicker.minimumDate = Date()
+        return datePicker
+    }()
+    
+    private let descriptionTextField: UITextView = {
+        let textField = UITextView()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont.systemFont(ofSize: 17)
+        textField.dataDetectorTypes = .all
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
